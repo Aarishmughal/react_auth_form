1b7a441663a9c3a3130b828016fa62fef93a79ad
RYTHMDX <110604636+Aarishmughal@users.noreply.github.com>
2025-07-12 23:31:38 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/mern_auth/", // Set base for GitHub Pages deployment
+    base: "/react_auth_form/", // Set base for GitHub Pages deployment
 });
